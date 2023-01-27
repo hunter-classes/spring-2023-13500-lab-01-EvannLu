79a71233feab2ba044f003424341be4a65e461bd
Evan Lu <evan.l60@myhunter.cuny.edu>
2023-01-26 20:57:37 -0500

    updated

--- Rec/leap.cpp
+++ Rec/leap.cpp
@@ -1,12 +1,29 @@
-/**
-    @file leap.cpp
-    @author Evan Lu
-    @date 01/26/2023
+/*
+Author: Evan Lu
+Course: CSCI-135
+Instructor: Professor Zamansky
+Assignment: Task C. A leap year or a common year?
 */
 
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World";
+    int year; 
+    std::cout << " Enter year: ";
+    std::cin >> year; 
+
+    if(year % 4 != 0 ){
+        std::cout << "Common year" << std::endl;
+    }
+    else if (year % 100 != 0){
+        std::cout << "Leap year" << std::endl;
+    }
+    else if(year % 400 != 0){
+        std::cout << "Common year" << std::endl;
+    }
+    else{
+        std::cout << "Leap year" << std::endl;
+    }
+    
     return 0;
 }
